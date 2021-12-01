a796494a7d056005f29ffa0b9eb9f00481ac8a95
francesmatthew <mfrance2@umbc.edu>
2021-12-01 02:24:40 -0500

    Increase utility of AOCHandler and finalize day01 solution

--- AOCHandler.py
+++ AOCHandler.py
@@ -4,22 +4,58 @@ Authors: Matthew Frances, Nicholas Conn
 Date: 11-30-2021
 Description: Handle program input and output on the filesystem
 """
-import os
 
 class AOCHandler:
-    def __init__(self, input_file, output_file):
-        self.input_file = input_file
-        self.output_file = output_file
+    BAD_INPUT_LINES = ['']
+
+    def __init__(self, input_file_name, output_file_name):
+        """
+        Initalize Advent of Code Handler with input and output filenames
+        """
+        self.input_file_name = input_file_name
+        self.output_file_name = output_file_name
 
     def get_input(self):
-        # Return a list string from the input file, newline-delimited
-        with open(self.input_file, 'r') as input:
-            raw_input = input.read()
-            raw_input = raw_input.replace('\r', '') # remove all carriage returns
-            return raw_input.split('\n',-1)
+        """
+        Return a list string from the input file, newline-delimited
+        """
+        with open(self.input_file_name, 'r') as input_file:
+            input = input_file.read()
+            input = input.replace('\r', '') # remove all carriage returns
+            input = input.split('\n', -1)
+        # remove empty strings (caused by trailing newline chars)
+        # must iterate from last-to-first when removing elements in-place
+        for index in range( (len(input)-1), -1, -1):
+            if input[index] in self.BAD_INPUT_LINES:
+                del input[index]
+        return input
 
-    def write_output(self, solution):
-        # Write the string `solution` to the output file
-        with open(self.output_file, 'w') as output:
-            output.write(solution)
+    def get_input_int(self):
+        """
+        Specialization of get_input() that returns a list of ints from the input file,
+        newline-delimited
+        This function may raise errors if input cannot be represented as an int
+        """
+        input_list = self.get_input()
+        for i in range(len(input_list)):
+            input_list[i] = int(input_list[i])
+        return input_list
+
+    def write_output(self, *args, **kwargs):
+        """
+        Write one or more arguments to the output file, one line per argument
+        Arguments must support conversion to string
+        kwargs:
+            verbose: (boolean) also print arguments to terminal
+        """
+        # default value for verbosity
+        if 'verbose' not in kwargs:
+            kwargs['verbose'] = False
+
+        with open(self.output_file_name, 'w') as output:
+            for arg in args:
+                arg = str(arg)
+                output.write(arg + '\n')
+                if kwargs['verbose']:
+                    print(arg)
             return
--- day01/day01.py
+++ day01/day01.py
@@ -2,22 +2,43 @@
 File: day01.py
 Authors: Matthew Frances, Nicholas Conn
 Date: 12-01-2021
-Description:
+Description: https://adventofcode.com/2021/day/1
 """
 import sys
-sys.path.append('..')
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from AOCHandler import AOCHandler
 
 def main():
     aoc_handler = AOCHandler("input.txt", "output.txt")
-    input = aoc_handler.get_input()
+    input = aoc_handler.get_input_int()
 
-    solution = 0
+    # count the number of times the current entry is greater than the previous entry
+    single_increase = 0
     for i in range(1, len(input)):
         if (input[i] > input[i-1]):
-            solution += 1
+            single_increase += 1
 
-    aoc_handler.write_output(str(solution))
+    # "consider sums of a three-measurement sliding window" i.e.:
+    # 199  A      
+    # 200  A B    
+    # 208  A B C  
+    # 210    B C D
+    # 200  E   C D
+    # 207  E F   D
+    # 240  E F G  
+    # 269    F G H
+    # 260      G H
+    # 263        H
+    # count the number of times the current sliding window is greater than the previous sliding window
+    triplet_increase = 0
+    for i in range(3, len(input)):
+        current_triplet = input[i] + input[i-1] + input[i-2]
+        prev_triplet = input[i-1] + input[i-2] + input[i-3]
+        if (current_triplet > prev_triplet):
+            triplet_increase += 1
+
+    aoc_handler.write_output(single_increase, triplet_increase, verbose=True)
 
 
 if __name__ == '__main__':
