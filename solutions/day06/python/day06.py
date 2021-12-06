1fa5a5521a17ec7284e2d1d5eaeefbc10a97ada2
francesmatthew <mfrance2@umbc.edu>
2021-12-06 01:16:57 -0500

    day06 solutions

--- solutions/day06/python/day06.py
+++ solutions/day06/python/day06.py
@@ -15,8 +15,32 @@ def main():
         return
     aoc_handler = AOCHandler(sys.argv[1])
 
-    if len(data_in) < 1:
+    fish_in = [int(num) for line in aoc_handler.get_input() for num in line.split(",") ]
+
+    if len(fish_in) < 1:
         raise BadInputException()
+
+    # count the number of lanternsfish with each possible timer value
+    fishes = {}
+    for i in range(9):
+        fishes[i] = 0
+    for fish in fish_in:
+        fishes[fish] += 1
+
+    # simulate fish creation
+    for _ in range(256):
+        new_fish = fishes[0]
+        # age fish
+        for age in range(1,9):
+            fishes[age-1] = fishes[age]
+        fishes[6] += new_fish
+        fishes[8] = new_fish
+
+    num_fish = 0
+    for count in fishes.values():
+        num_fish += count
+
+    aoc_handler.write_output(num_fish, verbose=True)
 
 if __name__ == '__main__':
     main()
