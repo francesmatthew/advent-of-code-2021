57922686fa330d5096f0b5ea6853dd6f119ce411
francesmatthew <mfrance2@umbc.edu>
2021-12-15 02:25:52 -0500

    day16 templates

---
+++ solutions/day16/python/day16.py
@@ -0,0 +1,36 @@
+""""
+File: day16.py
+Authors: Matthew Frances, Nicholas Conn, Peter Scrandis
+Date: 12-16-2021
+Description: https://adventofcode.com/2021/day/16
+"""
+from AOCHandler import AOCHandler, BadInputException
+import sys
+import os
+
+def p1(data):
+    pass
+
+def p2(data):
+    pass
+
+def main():
+    # get I/O identifier from command-line argument
+    if len(sys.argv) < 2:
+        print("Useage: python3 ./python/day16.py <I/O file identifier>")
+        return
+    aoc_handler = AOCHandler(sys.argv[1])
+
+    data_in = aoc_handler.get_input()
+    if len(data_in) < 1:
+        raise BadInputException()
+
+    # parse input data
+
+    solution1 = p1(data_in)
+    solution2 = p2(data_in)
+
+    aoc_handler.write_output(solution1, solution2, verbose=True)
+
+if __name__ == '__main__':
+    main()
